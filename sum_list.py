820a3e6a2973460c5c20f636259070d409d5e18f
Wandibba05 <emmanuel.wandibba@strathmore.edu>
2025-04-25 08:45:31 +0300

    Create sum_list.py
    Added sum_list.py with list summing logic

---
+++ sum_list.py
@@ -0,0 +1,8 @@
+# Finding the sum of all elements in the list
+def sum_list(numbers):
+  total = 0
+  for num in mumbers:
+    total += num # Adds each of the numbers to the total
+   return total
+# Example 
+print(sum_list([5,6,7,8,9])) #Output 35
