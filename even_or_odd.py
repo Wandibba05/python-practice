4ed14d4b8cb8c60b580a6289670609e6d4584d83
Wandibba05 <emmanuel.wandibba@strathmore.edu>
2025-04-25 08:58:25 +0300

    Create even_or_odd.py
    Added even_or_odd.py to check whether a number is odd or even

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+#The Fuction to check whether a number is even or odd
+def even_or_odd(num)
+if num %2 == 0:
+  return "Even"
+  else
+  return "false"
+  #Example
+  print(check_even_odd(6)) #Output is even since 6 % 2 ==0
+  print(check_even_odd(9)) #Output is odd since 9 % 2 ==1
